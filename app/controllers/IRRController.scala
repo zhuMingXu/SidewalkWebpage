232228444911bc713e274420f2b9ee3bf354a9cd
Manaswi Saha <manaswi@cs.umd.edu>
2017-08-22 19:04:27 -0400

    Added end-point for POST request
    Not complete - Initial commit

--- app/controllers/IRRController.scala
+++ app/controllers/IRRController.scala
@@ -1,6 +1,5 @@
 package controllers
 
-import java.sql.Timestamp
 import javax.inject.Inject
 
 import com.mohiva.play.silhouette.api.{Environment, Silhouette}
@@ -8,7 +7,8 @@ import com.mohiva.play.silhouette.impl.authenticators.SessionAuthenticator
 import controllers.headers.ProvidesHeader
 import models.clustering_session.ClusteringSessionTable
 import models.user.User
-import play.api.libs.json.Json
+import play.api.libs.json.{JsError, Json}
+import play.api.mvc.BodyParsers
 
 import scala.concurrent.Future
 
@@ -27,6 +27,28 @@ class IRRController @Inject()(implicit val env: Environment[User, SessionAuthent
 
     val json = Json.obj("labels" -> labels.foldLeft(Json.obj())(_ deepMerge _), "streets" -> streets.foldLeft(Json.obj())(_ deepMerge _))
     Future.successful(Ok(json))
+  }
+
+  def getDataForIRRForHits = UserAwareAction.async(BodyParsers.parse.json) { implicit request =>
+    println(request.body)
+    var submission = request.body//request.body.validate[List[String]]
+
+    println(submission)
+    Future.successful(Ok(Json.obj()))
+
+//    submission.fold(
+//      errors => {
+//        Future.successful(BadRequest(Json.obj("status" -> "Error", "message" -> JsError.toFlatJson(errors))))
+//      },
+//      submission => {
+//
+//        //    val streets = ClusteringSessionTable.getStreetGeomForIRR(routeId).map(_.toJSON)
+//        //    val labels = ClusteringSessionTable.getLabelsForIRR(hitId, routeId).map(_.toJSON)
+//        //    val json = Json.obj("labels" -> labels.foldLeft(Json.obj())(_ deepMerge _), "streets" -> streets.foldLeft(Json.obj())(_ deepMerge _))
+//
+//        Future.successful(Ok(Json.obj()))
+//      }
+//    )
   }
 
 }
