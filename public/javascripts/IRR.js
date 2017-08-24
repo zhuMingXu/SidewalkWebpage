581f3432e71b8ad3c5940efd3c7db380fa4aa547
misaugstad <michaelssaugstad@gmail.com>
2017-08-23 21:34:01 -0400

    IRR label counts at segment level now functional

--- public/javascripts/IRR.js
+++ public/javascripts/IRR.js
@@ -7,13 +7,14 @@ function setupIRR(data) {
     let labelsData = data.labels;
     let routes = [...new Set(streetsData.features.map(street => street.properties.route_id))]; // gets unique set of routes
     let output = [];
-    for(let i = 0; i < routes.length; i++) output[i] = [];
+    for(let i = 0; i < routes.length; i++) output[i] = {};
 
     for(let routeIndex = 0; routeIndex < routes.length; routeIndex++) {
         let currRoute = routes[routeIndex];
         let segs = streetsData.features.filter(street => street.properties.route_id === currRoute);
+        let streetOutput = [];
         for(let i = 0; i < segs.length; i++) {
-            output[routeIndex][i] =
+            streetOutput[i] =
                 {"CurbRamp": 0, "NoCurbRamp": 0, "NoSidewalk": 0,"Obstacle": 0, "Occlusion": 0, "SurfaceProblem": 0};
         }
 
@@ -23,7 +24,7 @@ function setupIRR(data) {
             let currLabel = labelsData.features[labIndex];
 
             // get closest street to this label
-            // http://turfjs.org/docs/#pointonline  (really read this documentation, this func has tons of useful output data)
+            // http://turfjs.org/docs/#pointonline
             let segIndex;
             let minDist = Number.POSITIVE_INFINITY;
             for (let i = 0; i < segs.length; i++) {
@@ -34,30 +35,43 @@ function setupIRR(data) {
             }
 
             // increment this street's count of labels (of this label type)
-            output[routeIndex][segIndex][currLabel.properties.label_type] += 1;
+            streetOutput[segIndex][currLabel.properties.label_type] += 1;
 
         }
+        output[routeIndex].street = streetOutput;
         console.log(output);
 
+
         // segment level
-        // TODO combine streets into a set of contiguous linestrings
+        // combine streets into a set of contiguous linestrings
         // http://turfjs.org/docs/#combine -- combines the different streets into a single MultiLineString
         // http://turfjs.org/docs/#lineintersect -- lets you know the points where two lines intersect
+        let combinedStreets = turf.combine(streetsData);
         console.log(turf.combine(streetsData));
 
 
-        let segDists = [5, 10]; // in meters
+        let segDists = [0.005, 0.01]; // in meters
         for(let segDistIndex = 0; segDistIndex < segDists.length; segDistIndex++) {
-            let segDist = segDists[routeIndex];
+            let segDist = segDists[segDistIndex];
 
-            // TODO split streets into a bunch of little segments based on segDist and length of each contiguous segment
+            // split streets into a bunch of little segments based on segDist and length of each contiguous segment
+            // TODO make sure that each contiguous segment is done separately
+            // TODO pick actual distance for each contiguous segment separately so that all segs are approx equal
             // http://turfjs.org/docs/#linechunk
+            let chunks = turf.lineChunk(combinedStreets, segDist);
+            console.log(chunks);
+
+            let segOutput = [];
+            for(let i = 0; i < chunks.features.length; i++) {
+                segOutput[i] =
+                    {"CurbRamp": 0, "NoCurbRamp": 0, "NoSidewalk": 0,"Obstacle": 0, "Occlusion": 0, "SurfaceProblem": 0};
+            }
 
             for(let labIndex = 0; labIndex < labelsData.features.length; labIndex++) {
                 let currLabel = labelsData.features[labIndex];
-                
-                // TODO get closest segment to this label
-                // http://turfjs.org/docs/#pointonline  (really read this documentation, this func has tons of useful output data)
+
+                // get closest segment to this label
+                // http://turfjs.org/docs/#pointonline
                 let segIndex;
                 let minDist = Number.POSITIVE_INFINITY;
                 for (let i = 0; i < segs.length; i++) {
@@ -68,11 +82,14 @@ function setupIRR(data) {
                 }
 
                 // TODO increment this segment's count of labels (of this label type)
+                segOutput[segIndex][currLabel.properties.label_type] += 1;
 
             }
+            output[routeIndex][String(segDist * 1000) + "_meter"] = segOutput;
         }
 
         // TODO save the output into some object
+        console.log(output);
 
     }
     
