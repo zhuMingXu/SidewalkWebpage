b71d0b5b95a001da52b3541a29c3caed6dda2ac5
misaugstad <michaelssaugstad@gmail.com>
2017-12-10 11:52:18 -0500

    added associated model for the new threshold table

---
+++ app/models/clustering_session/ClusteringSessionLabelTypeThresholdTable.scala
@@ -0,0 +1,54 @@
+package models.clustering_session
+
+/**
+  * Created by misaugstad on 12/10/17.
+  */
+
+import models.label.{LabelType, LabelTypeTable}
+import models.utils.MyPostgresDriver.simple._
+import play.api.Play.current
+
+import scala.slick.lifted.ForeignKeyQuery
+import scala.language.postfixOps
+
+case class ClusteringSessionLabelTypeThreshold(clusteringSessionLabelTypeThresholdId: Int,
+                                               clusteringSessionId: Int,
+                                               labelTypeId: Int,
+                                               clusteringThreshold: Float)
+
+/**
+  *
+  */
+class ClusteringSessionLabelTypeThresholdTable(tag: Tag) extends Table[ClusteringSessionLabelTypeThreshold](tag, Some("sidewalk"), "amt_volunteer_route") {
+  def clusteringSessionLabelTypeThresholdId = column[Int]("clustering_session_label_type_threshold_id", O.NotNull, O.PrimaryKey)
+  def clusteringSessionId = column[Int]("clustering_session_id", O.NotNull)
+  def labelTypeId = column[Int]("label_type_id", O.NotNull)
+  def clusteringThreshold = column[Float]("clustering_threshold", O.NotNull)
+
+  def * = (clusteringSessionLabelTypeThresholdId, clusteringSessionId, labelTypeId, clusteringThreshold) <>
+    ((ClusteringSessionLabelTypeThreshold.apply _).tupled, ClusteringSessionLabelTypeThreshold.unapply)
+
+  def clusteringSession: ForeignKeyQuery[ClusteringSessionTable, ClusteringSession] =
+    foreignKey("clustering_session_label_type_threshold_clustering_session_id_fkey", clusteringSessionId, TableQuery[ClusteringSessionTable])(_.clusteringSessionId)
+  def labelType: ForeignKeyQuery[LabelTypeTable, LabelType] =
+    foreignKey("clustering_session_label_type_threshold_clustering_session_id_fkey", labelTypeId, TableQuery[LabelTypeTable])(_.labelTypeId)
+
+}
+
+/**
+  * Data access object for the AMTVolunteerRoute table
+  */
+object AMTVolunteerRouteTable {
+  val db = play.api.db.slick.DB
+  val clusteringSessionLabelTypeThresholds = TableQuery[ClusteringSessionLabelTypeThresholdTable]
+  val clusteringSessions = TableQuery[ClusteringSessionTable]
+  val labelTypes = TableQuery[LabelTypeTable]
+
+
+  def save(newThresh: ClusteringSessionLabelTypeThreshold): Int = db.withTransaction { implicit session =>
+    val newId: Int =
+      (clusteringSessionLabelTypeThresholds returning clusteringSessionLabelTypeThresholds.map(_.clusteringSessionLabelTypeThresholdId)) += newThresh
+    newId
+  }
+}
+
